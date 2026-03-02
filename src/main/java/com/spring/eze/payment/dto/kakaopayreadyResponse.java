823d39e9e416c29d8c3637394011bcd3bfadcdac
ilellia <ilellia007@gmail.com>
2026-03-02 20:15:54 +0900

    [payment] feat : 관리자페이지, 카카오결제 추가

---
+++ src/main/java/com/spring/eze/payment/dto/kakaopayreadyResponse.java
@@ -0,0 +1,22 @@
+package com.spring.eze.payment.dto;
+
+public class kakaopayreadyResponse {
+
+	private String tid;
+	private String next_redirect_pc_url;
+	
+	public String getTid() {
+		return tid;
+	}
+	public void setTid(String tid) {
+		this.tid = tid;
+	}
+	public String getNext_redirect_pc_url() {
+		return next_redirect_pc_url;
+	}
+	public void setNext_redirect_pc_url(String next_redirect_pc_url) {
+		this.next_redirect_pc_url = next_redirect_pc_url;
+	}
+	
+	
+}
---
+++ src/main/java/com/spring/eze/payment/service/kakaopayService.java
@@ -0,0 +1,23 @@
+package com.spring.eze.payment.service;
+
+import com.spring.eze.payment.dto.kakaopayapproveResponse;
+import com.spring.eze.payment.dto.kakaopayorderRequest;
+import com.spring.eze.payment.dto.kakaopayreadyResponse;
+
+public interface kakaopayService {
+
+    // READY
+    kakaopayreadyResponse ready(
+            kakaopayorderRequest req,
+            String orderId,
+            long userId,
+            long reservationId
+    );
+
+    // APPROVE
+    kakaopayapproveResponse approve(
+            String pgToken,
+            String orderId,
+            long userId
+    );
+}
