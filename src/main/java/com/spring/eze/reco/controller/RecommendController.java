fe317020f7ff1a38cdb48c3fa3b1685f1c7577bd
GarlGaru <juon304@gmail.com>
2026-03-27 10:03:09 +0900

    [music] fix : api 연결, 제목 안나오던 문제 수정

--- src/main/java/com/spring/eze/reco/controller/RecommendController.java
+++ src/main/java/com/spring/eze/reco/controller/RecommendController.java
@@ -1,9 +1,7 @@
 package com.spring.eze.reco.controller;
 
 import com.spring.eze.common.LoginSessionHandler;
-import com.spring.eze.music.dto.SongDTO;
 import com.spring.eze.music.service.MusicService;
-import com.spring.eze.playlist.dto.PlaylistDTO;
 import com.spring.eze.reco.dto.SongCardDTO;
 import com.spring.eze.reco.service.RecommendService;
 import org.slf4j.Logger;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.spring.eze.common.LoginSessionHandler.SESSION_ERROR;
@@ -38,7 +35,8 @@ public class RecommendController {
      * */
     @ResponseBody
     @GetMapping("/music/personal-recommend")
-    public List<SongCardDTO> weekly(HttpServletRequest request) {
+    public List<SongCardDTO> recommendForUser(HttpServletRequest request) {
+        log.info("Reco Controller : recommendForUser");
         int userId = lsh.getUserIdFromSession(request);
         if (userId == SESSION_ERROR) {  // 로그인 안했으면 빈 리스트
             return List.of();
@@ -54,6 +52,7 @@ public class RecommendController {
     @ResponseBody
     @GetMapping("/music/personal-recommend-popular")
     public List<SongCardDTO> recommendPopularForUser(HttpServletRequest request) {
+        log.info("Reco Controller : recommendPopularForUser");
         int userId = lsh.getUserIdFromSession(request);
         if (userId == SESSION_ERROR) {  // 로그인 안했으면 주간 랭킹
             return service.convertToSongCardDTO(musicService.getweeklyRanking());
@@ -69,6 +68,7 @@ public class RecommendController {
     @ResponseBody
     @GetMapping("/music/personal-recommend-latest")
     public List<SongCardDTO> recommendLatestForUser(HttpServletRequest request) {
+        log.info("Reco Controller : recommendLatestForUser");
         int userId = lsh.getUserIdFromSession(request);
         if (userId == SESSION_ERROR) {  // 로그인 안했으면 그냥 최신곡
             return List.of();
